ed352d6a42c824da3e770ad24ee59456f9e36bbc
Alicia Wood <awood1115@Alicias-MacBook-Pro.local>
2017-04-03 00:24:01 -0400

    Created continuous threat status variable for European data and started European plots

--- analysis.r
+++ analysis.r
@@ -22,44 +22,55 @@ Taxonomy = read.table("Taxonomy.txt", header=T, sep = '\t', quote = "\"")
 EuroAmphib$ThreatBinary <- NA
 EuroAmphib$ThreatBinary[EuroAmphib$IUCN_Status_Vulnerable == 1 | EuroAmphib$IUCN_Status_Endangered == 1 | EuroAmphib$IUCN_Status_NearThreatened == 1 | EuroAmphib$IUCN_Status_CriticallyEndangered == 1] <- 1
 EuroAmphib$ThreatBinary[EuroAmphib$IUCN_Status_LeastConcern == 1] <- 0
+# Creating Continuous Threat Status Variable
+EuroAmphib$threat[EuroAmphib$IUCN_Status_LeastConcern == 1] <- 1
+EuroAmphib$threat[EuroAmphib$IUCN_Status_Vulnerable == 1] <- 2
+EuroAmphib$threat[EuroAmphib$IUCN_Status_NearThreatened == 1] <- 3
+EuroAmphib$threat[EuroAmphib$IUCN_Status_Endangered == 1] <- 5
+EuroAmphib$threat[EuroAmphib$IUCN_Status_CriticallyEndangered == 1] <- 6
+
 
 #Creating Habitat Breadth columns in both datasets
-EuroAmphib$Habitat_Forest <- apply(EuroAmphib[,c(123:131)], 1, sum)
-EuroAmphib$Habitat_Forest[EuroAmphib$Habitat_Forest > 0] <- 1
-EuroAmphib$Habitat_Savanna <- apply(EuroAmphib[,c(132:133)], 1, sum)
-EuroAmphib$Habitat_Savanna[EuroAmphib$Habitat_Savanna > 0] <- 1
-EuroAmphib$Habitat_Shrubland <- apply(EuroAmphib[,c(134:141)], 1, sum)
-EuroAmphib$Habitat_Shrubland[EuroAmphib$Habitat_Shrubland > 0] <- 1
-EuroAmphib$Habitat_Grassland <- apply(EuroAmphib[,c(142:148)], 1, sum)
-EuroAmphib$Habitat_Grassland[EuroAmphib$Habitat_Grassland > 0] <- 1
-EuroAmphib$Habitat_Wetlands <- apply(EuroAmphib[,c(149:164)], 1, sum)
-EuroAmphib$Habitat_Wetlands[EuroAmphib$Habitat_Wetlands > 0] <- 1
-EuroAmphib$Habitat_RockyAreas[EuroAmphib$Habitat_RockyAreas_inland_cliffs_moutain_peaks == 1] <- 1
-EuroAmphib$Habitat_RockyAreas[EuroAmphib$Habitat_RockyAreas_inland_cliffs_moutain_peaks == 0] <- 0
-EuroAmphib$Habitat_Caves <- apply(EuroAmphib[,c(166:167)], 1, sum)
-EuroAmphib$Habitat_Caves[EuroAmphib$Habitat_Caves > 0] <- 1
-EuroAmphib$Habitat_Deserts <- apply(EuroAmphib[,c(168:170)], 1, sum)
-EuroAmphib$Habitat_Deserts[EuroAmphib$Habitat_Deserts > 0] <- 1
-EuroAmphib$Habitat_ArtificialTerrestrial <- apply(EuroAmphib[,c(171:176)], 1, sum)
-EuroAmphib$Habitat_ArtificialTerrestrial[EuroAmphib$Habitat_ArtificialTerrestrial > 0] <- 1
-EuroAmphib$Habitat_Other[EuroAmphib$Habitat_Other_dunes == 1] <- 1
-EuroAmphib$Habitat_Other[EuroAmphib$Habitat_Other_dunes == 0] <- 0
+EuroAmphib$Forest <- apply(EuroAmphib[,c(123:131)], 1, sum)
+EuroAmphib$Forest[EuroAmphib$Forest > 0] <- 1
+EuroAmphib$Savanna <- apply(EuroAmphib[,c(132:133)], 1, sum)
+EuroAmphib$Savanna[EuroAmphib$Savanna > 0] <- 1
+EuroAmphib$Shrubland <- apply(EuroAmphib[,c(134:141)], 1, sum)
+EuroAmphib$Shrubland[EuroAmphib$Shrubland > 0] <- 1
+EuroAmphib$Grassland <- apply(EuroAmphib[,c(142:148)], 1, sum)
+EuroAmphib$Grassland[EuroAmphib$Grassland > 0] <- 1
+EuroAmphib$Wetlands <- apply(EuroAmphib[,c(149:164)], 1, sum)
+EuroAmphib$Wetlands[EuroAmphib$Wetlands > 0] <- 1
+EuroAmphib$RockyAreas[EuroAmphib$Habitat_RockyAreas_inland_cliffs_moutain_peaks == 1] <- 1
+EuroAmphib$RockyAreas[EuroAmphib$Habitat_RockyAreas_inland_cliffs_moutain_peaks == 0] <- 0
+EuroAmphib$Caves <- apply(EuroAmphib[,c(166:167)], 1, sum)
+EuroAmphib$Caves[EuroAmphib$Caves > 0] <- 1
+EuroAmphib$Deserts <- apply(EuroAmphib[,c(168:170)], 1, sum)
+EuroAmphib$Deserts[EuroAmphib$Deserts > 0] <- 1
+EuroAmphib$ArtificialTerrestrial <- apply(EuroAmphib[,c(171:176)], 1, sum)
+EuroAmphib$ArtificialTerrestrial[EuroAmphib$ArtificialTerrestrial > 0] <- 1
+EuroAmphib$Other[EuroAmphib$Habitat_Other_dunes == 1] <- 1
+EuroAmphib$Other[EuroAmphib$Habitat_Other_dunes == 0] <- 0
 
 EuroAmphib$HabitatBreadth <- apply(EuroAmphib[,c(256,265)], 1, sum)
 NAfrogs$HabitatBreadth <- apply(NAfrogs[,c(22:33)], 1, sum)
 
-#Creating singular clutch size column
+#Condensing mean max and min Clutch Size into 1 mean Clutch Size column for NA dataset
 NAfrogs$ClutchSize <- ifelse(is.na(NAfrogs$MeanClutchSize), ifelse(is.na(NAfrogs$MaxClutchSize), ifelse(is.na(NAfrogs$MinClutchSize), NA, NAfrogs$MinClutchSize), ifelse(is.na(NAfrogs$MinClutchSize), NAfrogs$MaxClutchSize, rowMeans(NAfrogs[,c("MinClutchSize", "MaxClutchSize")]))), NAfrogs$MeanClutchSize)
+#Europe dataset already in 1 column named number_of_eggs_or_offspring
 
-#Creating singularSVLunspecified column
+#Condensing mean max and min SVL unspecified into 1 column for NA dataset
 NAfrogs$SVLunspecified <- ifelse(is.na(NAfrogs$MeanSVL_Unspecified_mm), ifelse(is.na(NAfrogs$MaxSVL_Unspecified_mm), ifelse(is.na(NAfrogs$MinSVL_Unspecified_mm), NA, NAfrogs$MinSVL_Unspecified_mm), ifelse(is.na(NAfrogs$MinSVL_Unspecified_mm), NAfrogs$MaxSVL_Unspecified_mm, rowMeans(NAfrogs[,c("MinSVL_Unspecified_mm", "MaxSVL_Unspecified_mm")]))), NAfrogs$MeanSVL_Unspecified_mm)
-#Creating singular SVLfemale column
+#Condensing mean max and min SVL female into 1 column
 NAfrogs$SVLfemale <- ifelse(is.na(NAfrogs$MeanSVL_Female_mm), ifelse(is.na(NAfrogs$MaxSVL_Female_mm), ifelse(is.na(NAfrogs$MinSVL_Female_mm), NA, NAfrogs$MinSVL_Female_mm), ifelse(is.na(NAfrogs$MinSVL_Female_mm), NAfrogs$MaxSVL_Female_mm, rowMeans(NAfrogs[,c("MinSVL_Female_mm", "MaxSVL_Female_mm")]))), NAfrogs$MeanSVL_Female_mm)
-#Creating singular SVLmale column
+#Condensing mean max and min SVL male into 1 column
 NAfrogs$SVLmale <- ifelse(is.na(NAfrogs$MeanSVL_Male_mm), ifelse(is.na(NAfrogs$MaxSVL_Male_mm), ifelse(is.na(NAfrogs$MinSVL_Male_mm), NA, NAfrogs$MinSVL_Male_mm), ifelse(is.na(NAfrogs$MinSVL_Male_mm), NAfrogs$MaxSVL_Male_mm, rowMeans(NAfrogs[,c("MinSVL_Male_mm", "MaxSVL_Male_mm")]))), NAfrogs$MeanSVL_Male_mm)
-#Creating singular SVL overall column
+#Condensing male female and unspecified SVL into 1 final mean SVL column
 NAfrogs$SVLMFmean <- rowMeans(NAfrogs[,c("SVLfemale", "SVLmale")])
 NAfrogs$SVL <- ifelse(!is.na(NAfrogs$SVLunspecified) & !is.na(NAfrogs$SVLfemale) & !is.na(NAfrogs$SVLmale), rowMeans(NAfrogs[,c("SVLunspecified", "SVLMFmean")]), ifelse(is.na(NAfrogs$SVLunspecified), ifelse(is.na(NAfrogs$SVLfemale), ifelse(is.na(NAfrogs$SVLmale), NA, NAfrogs$SVLmale), ifelse(is.na(NAfrogs$SVLmale), NAfrogs$SVLfemale, NAfrogs$SVLMFmean)), NAfrogs$SVLunspecified))
+#Mean SVL alredy in 1 column in Euro dastaset named SVL_unspecified_mm
+
+
 
 # Threat status ordinal values
 threat = data.frame(IUCN_Threat_Status = c('LC', 'VU', 'NT', 'TH', 'EN', 'CR', 'EW'),
@@ -123,7 +134,8 @@ MultiLMa = lm(Anaxyrus$threat ~ Anaxyrus$SVL + Anaxyrus$ClutchSize + Anaxyrus$Ha
 summary(MultiLMa)
 
 
-
+### Next group by order then plot European frogs and salamanders
+plot(EuroAmphib$SVL_Unspecified_mm, EuroAmphib$threat, xlab = "Mean SVL (mm)", ylab = "Threat Status", main = "Threat Status vs Mean SVL", col = "darkgreen", pch = 16)
 
 
 #### Paired Analysis
